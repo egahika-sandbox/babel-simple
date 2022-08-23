375491737ab4b55d77f39de9119355fa835b6718
hikae <contact@egahika.dev>
2022-08-23 21:19:43 +0900

    pipeline-operator+ts

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+var _ref, _claireRedfield;
+
+_ref = (_claireRedfield = 'claire redfield', _claireRedfield[0].toUpperCase() + _claireRedfield.substring(1) // 'Claire redfield'
+), console.log(_ref);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+'claire redfield' 
+  |> ((name: string) => name[0].toUpperCase() + name.substring(1)) // 'Claire redfield'
+  |> console.log
